2f354f3370e1eb658155f34a3f56262de45abdc3
mirronmahamudlima <mijan818629@gmail.com>
2024-01-11 12:03:10 +0600

    class-06

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+<img src="{{asset('images/Screenshot 2024-01-09 232613.png')}}" alt="logo" class="h-[45px]" />
---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="footer">
+    <p class="text-sm">
+      Copyright © {{now()->year}}
+      <a class="text-primary-500 hover:underline" href="https://github.com/mirronmahamudlima/ecommers-dashboard" target="_blank">
+        Admin Dashboard
+      </a>
+    </p>
+
+    <a href="https://github.com/mirronmahamudlima/ecommers-dashboard" target="_blank" class="flex items-center gap-1 text-sm">
+        "Like it? ⭐ Give us a star on 
+      <i class="text-danger-500" data-feather="github" height="1em" width="1em"></i>
+    </a>
+  </footer>
